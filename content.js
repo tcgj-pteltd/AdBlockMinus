49e9a1ac90ea874772fec773449d36051186bbae
tcgj <terenceblur0410@gmail.com>
2022-01-07 23:07:16 +0800

    Fix sidebar ad

--- content.js
+++ content.js
@@ -37,93 +37,8 @@ function getRandomSidebarAd(min = 1, max = 1) {
     }
 }
 
-function addOverlayAd() {
-    var vidURL = chrome.runtime.getURL("woolooloo.mp4");
-    var div = document.createElement("DIV");
-    div.id = "overlay-ad";
-    var vid = document.createElement("VIDEO");
-
-    vid.setAttribute("controls", "true");
-    vid.setAttribute("disablePictureInPicture", "true");
-    vid.setAttribute("controlsList", "nodownload noplaybackrate nopictureinpicture");
-
-    vid.id = "ad-media";
-    vid.src = vidURL;
-    div.appendChild(vid);
-    var button = document.createElement("button");
-    button.id = "close";
-    button.onclick = removeOverlayAd;
-    // hide the button to close the video, until user finishes watching the video
-    button.style.display = "none";
-    button.textContent = "Close Ad";
-    div.appendChild(button);
-    document.body.appendChild(div);
-    adList.push(div);
-}
-
-function removeOverlayAd() {
-    if (document.getElementById("overlay-ad")) {
-        document.getElementById("overlay-ad").remove();
-    }
-}
-
-function addHeaderAd() {
-    const imageHref = getRandomAd();
-    var imgURL = chrome.runtime.getURL(imageHref);
-    var div = document.createElement("DIV");
-    div.id = "header";
-    var img = document.createElement("IMG");
-    img.id = "ad-media";
-    img.src = imgURL;
-    img.alt = "Header Ad";
-    div.appendChild(img);
-    document.body.prepend(div);
-    adList.push(div);
-
-    setTimeout(() => {
-        const innerImageHref = getRandomAd();
-        var newImgURL = chrome.runtime.getURL(innerImageHref);
-        var div2 = document.createElement("DIV");
-        div2.id = "header";
-        var img2 = document.createElement("IMG");
-        img2.id = "ad-media";
-        img2.src = newImgURL;
-        div2.appendChild(img2);
-
-        const sectionHeader = document.querySelector(".section-header");
-        sectionHeader?.prepend(div2);
-
-        const mainContent = document.querySelector(".main-content");
-        mainContent?.prepend(div2);
-        adList.push(div2);
-    }, 1000);
-}
-
-function addSidebarAd() {
-    var div = document.createElement("div");
-    div.id = "wrap";
-    // Move the body's children into this wrapper
-    while (document.body.firstChild) {
-        div.appendChild(document.body.firstChild);
-    }
-    // Append the wrapper to the body
-    document.body.appendChild(div);
-
-    const imageHref = getRandomSidebarAd();
-    var imgURL = chrome.runtime.getURL(imageHref);
-    var div = document.createElement("DIV");
-    div.id = "sidenav";
-    var img = document.createElement("IMG");
-    img.id = "ad-media";
-    img.src = imgURL;
-    img.alt = "Sidebar Ad";
-    div.appendChild(img);
-    document.body.prepend(div);
-    adList.push(img);
-}
-
 function preventVideoSeek() {
-    var video = document.getElementById('ad-media');
+    var video = document.getElementById('add-media');
     var supposedCurrentTime = 0;
     video.addEventListener('timeupdate', function () {
         if (!video.seeking) {
@@ -147,6 +62,82 @@ function preventVideoSeek() {
         // allow user to close the video
         document.getElementById("close").style.display = "block";
     });
+}
+
+function addOverlayAd() {
+    var vidURL = chrome.runtime.getURL("woolooloo.mp4");
+    var div = document.createElement("DIV");
+    div.id = "add-overlay";
+    var vid = document.createElement("VIDEO");
+
+    vid.setAttribute("controls", "true");
+    vid.setAttribute("disablePictureInPicture", "true");
+    vid.setAttribute("controlsList", "nodownload noplaybackrate nopictureinpicture");
+
+    vid.id = "add-media";
+    vid.src = vidURL;
+    div.appendChild(vid);
+    var button = document.createElement("button");
+    button.id = "close";
+    button.onclick = removeOverlayAd;
+    // hide the button to close the video, until user finishes watching the video
+    button.style.display = "none";
+    button.textContent = "Close Ad";
+    div.appendChild(button);
+    document.body.appendChild(div);
+    adList.push(div);
+}
+
+function removeOverlayAd() {
+    if (document.getElementById("add-overlay")) {
+        document.getElementById("add-overlay").remove();
+    }
+}
+
+function addHeaderAd() {
+    const imageHref = getRandomAd();
+    var imgURL = chrome.runtime.getURL(imageHref);
+    var div = document.createElement("DIV");
+    div.id = "add-header";
+    var img = document.createElement("IMG");
+    img.id = "add-media";
+    img.src = imgURL;
+    img.alt = "Header Ad";
+    div.appendChild(img);
+    document.body.prepend(div);
+    adList.push(div);
+
+    setTimeout(() => {
+        const innerImageHref = getRandomAd();
+        var newImgURL = chrome.runtime.getURL(innerImageHref);
+        var div2 = document.createElement("DIV");
+        div2.id = "add-subheader";
+        var img2 = document.createElement("IMG");
+        img2.id = "add-media";
+        img2.src = newImgURL;
+        div2.appendChild(img2);
+
+        const sectionHeader = document.querySelector(".section-header");
+        if (sectionHeader)
+            sectionHeader.prepend(div2);
+        else
+            document.querySelector(".main-content")?.prepend(div2);
+        adList.push(div2);
+    }, 1000);
+}
+
+function addSidebarAd() {
+    const imageHref = getRandomSidebarAd();
+    var imgURL = chrome.runtime.getURL(imageHref);
+    var div = document.createElement("DIV");
+    div.id = "add-side";
+    var img = document.createElement("IMG");
+    img.id = "add-media";
+    img.src = imgURL;
+    img.alt = "Sidebar Ad";
+    div.appendChild(img);
+    document.body.prepend(div);
+    adList.push(div);
 }
 
 function loadAddAd() {
